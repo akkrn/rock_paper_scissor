f1da80ac9c033cf65e1740a1c35c1ae9181acf2b
Anton Kokorin <ant.kkrn@gmail.com>
2023-06-15 16:03:18 +0300

    add bot.py

--- config_data/config.py
+++ config_data/config.py
@@ -4,7 +4,7 @@ from environs import Env
 
 @dataclass
 class TgBot:
-    token: str            # Токен для доступа к телеграм-боту
+    token: str  # Токен для доступа к телеграм-боту
 
 
 @dataclass
@@ -15,4 +15,4 @@ class Config:
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
-    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
+    return Config(tg_bot=TgBot(token=env("BOT_TOKEN")))
